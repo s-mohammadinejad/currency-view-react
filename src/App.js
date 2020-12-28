3a505ccbf19064c64920bcf5fbf7c59f6f044f67
Mahsa Mohammadi Nejad <s.mohammadinejad.7@gmail.com>
2020-12-29 00:26:29 +0330

    part1:creat

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+//const api = "";
+
+function App() {
+  return (
+    <div className="App">
+     <main>
+       <div className="currency">
+         <p>currency</p>
+         <p>currency</p>
+         <p>currency</p>
+         <p>currency</p>
+       </div>
+     </main>
+    </div>
+  );
+}
+
+export default App;
