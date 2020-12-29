365c7859ecb8db59c1e475e64dd93c92b29e27c1
Mahsa Mohammadi Nejad <s.mohammadinejad.7@gmail.com>
2020-12-29 15:39:38 +0330

    part2: date-box and some css code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import React from 'react';
+import currency from './currency';
 //const api = "";
+var jalaali = require('jalaali-js')
+const dateBuilder = (d) => {
+  let months = ["January" , "February" , "March" , "April" , "May", "June" , "July" , "August" , "September" , "October" , "November" , "December"];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+  
+  return ` ${year} ${month} ${date} `
+}
+const solardatebuilder=(r) => {
+  let months = ["فروردین","اردیبهشت","خرداد","تیر","مرداد","شهریور","مهر","آبان","آذر","دی","بهمن","اسفند"];
+  var solar= jalaali.toJalaali(r.getFullYear(), r.getMonth(), r.getDate())
+  let month = months[solar.jm];
+  return `${month} ${solar.jy}  `
+}
+const daybuilder= (d) => {
+  let days = ["یک شنبه" , "دوشنبه" , "سه شنبه" , "چهارشنبه" , "پنج شنبه" , "جمعه" , "شنبه"];
+
+    let day = days[d.getDay()];
+    var solar= jalaali.toJalaali(d.getFullYear(), d.getMonth(), d.getDate())
+    return `${day} ${solar.jd}`
+}
+
+
 
 function App() {
   return (
     <div className="App">
-     <main>
-       <div className="currency">
-         <p>currency</p>
-         <p>currency</p>
-         <p>currency</p>
-         <p>currency</p>
-       </div>
-     </main>
+      <main>
+
+      <div className="date-box">
+      <div>
+        {daybuilder(new Date())}
+        {solardatebuilder(new Date())}
+        </div>
+     <div className="date">{dateBuilder(new Date())}</div>
+     </div>
+      </main>
     </div>
   );
 }
