47985115e8cf8f402afc98bdfab1c00c645b67fd
z-rafatnia <s.mohammadinejad.7@gmail.com>
2020-12-30 22:15:03 +0330

    part3:creat currencys

--- src/currency.js
+++ src/currency.js
@@ -3,19 +3,9 @@ import React from 'react'
 function currency() {
     return (
         <div>
-           <div className="currency__name">
-             <h1> dollar </h1> 
-            </div>
-            <div className="currencu__sell">
-                <p> 123456789</p>
-            </div>
-            <div className="cyrrency__buy">
-                <p>987654321</p>
-            </div>
-            <div className="currency__change">
-              
+           
 
-            </div>
+           
         </div>
     )
 }
